dd40cfbfdbc72ecb8911f71f3a9f57b891a55f08
angsanch <angsanch@student.42madrid.com>
2024-08-05 03:29:48 +0200

    FIX: file management

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ static int	get_fds(t_pipex *pipex, char *in, char *out)
 	pipex->ifd = open(in, O_RDONLY);
 	if (pipex->ifd < 0)
 		return (0);
-	pipex->ofd = open(out, O_WRONLY | O_CREAT);
+	pipex->ofd = open(out, O_WRONLY | O_CREAT, 0664);
 	if (pipex->ofd < 0)
 	{
 		close(pipex->ifd);
@@ -26,31 +26,26 @@ static int	get_fds(t_pipex *pipex, char *in, char *out)
 	return (1);
 }
 
-static int	run(t_pipex *pipex, int argc, char **argv)
-{
-	if (!get_fds(pipex, argv[1], argv[argc - 1]))
-		return (0);
-	if (!execution(pipex))
-		return (0);
-	return (1);
-}
-
 static int	pipex(int argc, char **argv, char **env)
 {
 	t_pipex	*pipex;
 	int		status;
-	int		exit_code;
 
 	pipex = pipex_create(argc, argv, env, argc - 3);
 	if (pipex == NULL)
 		return (84);
 	pipex->env = env;
-	status = run(pipex, argc, argv);
-	exit_code = pipex->command[pipex->command_amount - 1].exit_status;
-	pipex_destroy(pipex);
-	if (!status)
+	if (!get_fds(pipex, argv[1], argv[argc - 1]))
+	{
+		pipex_destroy(pipex);
 		return (84);
-	return (exit_code);
+	}
+	if (!execution(pipex))
+		status = 1;
+	else
+		status = pipex->command[pipex->command_amount - 1].exit_status;
+	pipex_destroy(pipex);
+	return (status);
 }
 
 int	main(int argc, char **argv, char **env)
